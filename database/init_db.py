74d6e35f16fdee2e8a2d3817680ecbb9f0ab31ec
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2026-04-06 19:03:40 +0800

    Update init_db.py

--- database/init_db.py
+++ database/init_db.py
@@ -1,10 +1,15 @@
-""" database/init_db.py Membuat database dan tabel """
+"""database/init_db.py - Membuat database dan tabel"""
 
-from database.db import engine from database.models import Base
+from database.db import engine
+from database.models import Base
 
-def init_database(): print("Membuat database...")
 
-Base.metadata.create_all(bind=engine) print("Database siap") 
+def init_database():
+    """Membuat semua tabel di database"""
+    print("Membuat database...")
+    Base.metadata.create_all(bind=engine)
+    print("Database siap")
 
-if name == "main": init_database()
 
+if __name__ == "__main__":
+    init_database()
